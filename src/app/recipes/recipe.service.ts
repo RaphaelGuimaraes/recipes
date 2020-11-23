2914ba742b327330060de754eb5b55017c5a397c
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-11-23 20:48:59 -0300

    saving and fetching data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,26 +9,27 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Shrimps and Salsa',
-      'Put some shrimps with salsa',
-      'https://www.lifesambrosia.com/wp-content/uploads/Spicy-Grilled-Shrimp-Recipe-Photo-2-795x1200.jpg',
-      [
-        new Ingredient('Shrimp', 5),
-        new Ingredient('Salsa', 5),
-      ]
-    ),
-    new Recipe(
-      'Pudim',
-      'Its a mother fucker pudim',
-      'https://www.receitas-sem-fronteiras.com/uploads/media/pudim-30.jpg?1396917115',
-      [
-        new Ingredient('Milk', 1),
-        new Ingredient('Sugar', 1),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Shrimps and Salsa',
+  //     'Put some shrimps with salsa',
+  //     'https://www.lifesambrosia.com/wp-content/uploads/Spicy-Grilled-Shrimp-Recipe-Photo-2-795x1200.jpg',
+  //     [
+  //       new Ingredient('Shrimp', 5),
+  //       new Ingredient('Salsa', 5),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Pudim',
+  //     'Its a mother fucker pudim',
+  //     'https://www.receitas-sem-fronteiras.com/uploads/media/pudim-30.jpg?1396917115',
+  //     [
+  //       new Ingredient('Milk', 1),
+  //       new Ingredient('Sugar', 1),
+  //     ]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
@@ -56,6 +57,11 @@ export class RecipeService {
 
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 }
